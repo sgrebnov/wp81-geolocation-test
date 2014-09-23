a51965597c4835d7cc8452a98fe37b6820c2b2b1
sgrebnov <v-segreb@microsoft.com>
2014-09-23 13:34:36 +0400

    window.navigator.geolocation test for wp81

---
+++ geolocationTestApp/geolocationTestApp/geolocationTestApp.Shared/js/default.js
@@ -0,0 +1,81 @@
+﻿(function () {
+    "use strict";
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+
+            args.setPromise(WinJS.UI.processAll().
+                done(function () {
+
+                    // Add an event handler to the button.
+                    document.querySelector("#watchLoc").addEventListener("click", watchloc);
+
+                    // Add an event handler to the button.
+                    document.querySelector("#stopWatching").addEventListener("click", stopwatching);
+
+                }));
+        }
+    };
+
+    var loc = null;
+    var watchId;
+
+    function watchloc() {
+        if (loc == null) {
+            loc = window.navigator.geolocation;
+        }
+
+        if (loc != null) {
+            watchId = loc.watchPosition(successCallback, errorCallback);
+        }
+
+        //new Windows.Devices.Geolocation.Geolocator().getGeopositionAsync().then(
+        //        function (pos) {
+                    
+        //        },
+        //        function (err) {
+                    
+        //        }
+        //    );
+
+    }
+
+    function stopwatching() {
+        loc.clearWatch(watchId);
+    }
+
+    function successCallback(pos) {
+        document.getElementById('latitude').innerHTML = pos.coords.latitude;
+        document.getElementById('longitude').innerHTML = pos.coords.longitude;
+        document.getElementById('accuracy').innerHTML = pos.coords.accuracy;
+    }
+
+    function errorCallback(error) {
+        var strMessage = "";
+
+        // Check for known errors
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                strMessage = "Access to your location is turned off. " +
+                    "Change your settings to turn it back on.";
+                break;
+            case error.POSITION_UNAVAILABLE:
+                strMessage = "Data from location services is " +
+                    "currently unavailable.";
+                break;
+            case error.TIMEOUT:
+                strMessage = "Location could not be determined " +
+                    "within a specified timeout period.";
+                break;
+            default:
+                break;
+        }
+        document.getElementById("status").innerHTML = strMessage;
+    }
+
+
+    app.start();
+})();
